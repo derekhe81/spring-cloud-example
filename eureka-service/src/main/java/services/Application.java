3b034e63b0d69d216507dc88ae024f0a92379cca
hexuan <hexuan@ubuntu>
2017-05-03 03:06:04 -0700

    init project

---
+++ eureka-service/src/main/java/services/Application.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author derekhe
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
